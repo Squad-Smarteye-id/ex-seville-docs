e4260326f36cc538a71c887092b2d80f583194dd
ITDRIDEVELOPMENT <melodyslsbll90@gmail.com>
2024-03-07 20:54:40 +0700

    ex seville documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
+  logo: <span>Seville</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'TITIT Template',
   },
 }
 
